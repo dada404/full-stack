3bff8234cb14940794d695a04c16abaf54bbb86f
dada <dada404@foxmail.com>
2023-02-16 01:06:44 +0800

    1.8: unicafe step3

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,24 +6,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  class Statistic {
+    constructor(name, number) {
+      this.name = name;
+      this.number = number;
+    }
+  }
+  const statistics = [
+    new Statistic("good", good),
+    new Statistic("netural", neutral),
+    new Statistic("bad", bad),
+    new Statistic("all", good + neutral + bad),
+    new Statistic("average", (good - bad) / (good + neutral + bad)),
+    new Statistic(
+      "postive",
+      ((1.0 * good) / (good + neutral + bad)) * 100 + "%"
+    ),
+  ];
   return (
     <div>
       <h1>give feedback</h1>
       <Button text="good" handleClick={() => setGood(good + 1)} />
       <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
       <Button text="bad" handleClick={() => setBad(bad + 1)} />
-      <h1>statistics</h1>
-      <FeedBackState text="good" time={good}></FeedBackState>
-      <FeedBackState text="neutral" time={neutral}></FeedBackState>
-      <FeedBackState text="bad" time={bad}></FeedBackState>
-      <FeedBackState
-        text="average"
-        time={(good - bad) / (good + neutral + bad)}
-      ></FeedBackState>
-      <FeedBackState
-        text="positive"
-        time={good / (good + neutral + bad)}
-      ></FeedBackState>
+      <Statistics name="Statistics" statistics={statistics}></Statistics>
     </div>
   );
 };
@@ -33,14 +39,31 @@ const App = () => {
 //     <Button handleClick={state.handleClick} text={state.text} key={index} />
 //   ));
 // };
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>;
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
 };
 
-const FeedBackState = (props) => (
+const FeedBackStatistic = ({ text, number }) => (
   <div>
-    {props.text} {props.time}
+    {text} {number}
   </div>
 );
+const FeedBackStatistics = ({ statistics }) => {
+  return statistics.map((statistic) => (
+    <FeedBackStatistic
+      text={statistic.name}
+      number={statistic.number}
+    ></FeedBackStatistic>
+  ));
+};
+const Statistics = ({ name, statistics }) => {
+  console.log(name, statistics);
+  return (
+    <>
+      <h1>{name}</h1>
+      <FeedBackStatistics statistics={statistics}></FeedBackStatistics>
+    </>
+  );
+};
 
 export default App;
