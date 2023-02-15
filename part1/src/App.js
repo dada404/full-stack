4549e8e2addd1a043118e0e574d178c575cb0004
dada <dada404@foxmail.com>
2023-02-15 22:55:09 +0800

    exercise 1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,24 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
   return (
     <div>
       <Header course={course}> </Header>
-      <Content part1={part1} part2={part2} part3={part3}></Content>
-      <Total
-        total={part1.exercises + part2.exercises + part3.exercises}
-      ></Total>
+      <Content parts={parts}></Content>
+      <Total parts={parts}></Total>
     </div>
   );
 };
@@ -28,13 +27,9 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
-    </>
-  );
+  return props.parts.map((part, index) => (
+    <Part part={part} key={index}></Part>
+  ));
 };
 
 const Part = (props) => {
@@ -46,7 +41,9 @@ const Part = (props) => {
 };
 
 const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>;
+  const sum = props.parts.reduce((ac, cur) => ac + cur.exercises, 0);
+
+  return <p>Number of exercises {sum}</p>;
 };
 
 export default App;
