ae19fc7cf5bd6cbdd2806ebb9717a91da9993117
dada <dada404@foxmail.com>
2023-02-19 12:22:10 +0800

    2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,27 +1,58 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const name = "Web development curriculum";
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <>
+      <h1>{name}</h1>
+      {courses.map((course) => (
+        <Course course={course} id={course.id} />
+      ))}
+    </>
+  );
 };
 
 const Course = ({ course }) => {
@@ -35,7 +66,7 @@ const Course = ({ course }) => {
 };
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h2>{props.course}</h2>;
 };
 const Content = (props) => {
   return props.parts.map((part) => <Part part={part} key={part.id}></Part>);
@@ -52,7 +83,7 @@ const Part = (props) => {
 const Total = (props) => {
   const sum = props.parts.reduce((ac, cur) => ac + cur.exercises, 0);
 
-  return <p>Number of exercises {sum}</p>;
+  return <b>Number of exercises {sum}</b>;
 };
 
 export default App;
