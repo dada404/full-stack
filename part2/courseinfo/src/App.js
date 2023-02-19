2d05175bbcd8f503d6a92c898f936050b5c549a3
dada <dada404@foxmail.com>
2023-02-19 12:27:07 +0800

    2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,3 +1,5 @@
+import Course from "./component/Course";
+
 const App = () => {
   const name = "Web development curriculum";
   const courses = [
@@ -53,37 +55,6 @@ const App = () => {
       ))}
     </>
   );
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name}> </Header>
-      <Content parts={course.parts}></Content>
-      <Total parts={course.parts}></Total>
-    </div>
-  );
-};
-
-const Header = (props) => {
-  return <h2>{props.course}</h2>;
-};
-const Content = (props) => {
-  return props.parts.map((part) => <Part part={part} key={part.id}></Part>);
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Total = (props) => {
-  const sum = props.parts.reduce((ac, cur) => ac + cur.exercises, 0);
-
-  return <b>Number of exercises {sum}</b>;
 };
 
 export default App;
